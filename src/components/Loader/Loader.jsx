ba1fe75b5beef3d9518559ea527f47751a7b0902
YuriiKabo <yourrocksmirnov@gmail.com>
2023-05-19 12:38:02 +0300

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,21 +1,18 @@
 import { Container } from '@mui/material';
-import { CirclesWithBar } from 'react-loader-spinner';
+import { ColorRing } from 'react-loader-spinner';
 
 export function Loader() {
-    return (
-        <Container sx={{p:'40px',opacity:'.6'}}
-        ><CirclesWithBar
-                height="50%"
-                width="50%"
-                color="#11790f"
-                wrapperStyle={{}}
-                wrapperClass=""
-                visible={true}
-                outerCircleColor=""
-                innerCircleColor=""
-                barColor=""
-                ariaLabel='circles-with-bar-loading'
-            />
-        </Container>
-    );
-};
+  return (
+    <Container sx={{ p: '40px', opacity: '.6' }}>
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />
+    </Container>
+  );
+}
